7ac3d3ffb02433d3e430ece314f8845f0c196518
Jafnee <jafneejesmee@gmail.com>
2015-08-23 02:59:19 +0800

    Add initial Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'css/style.css' : 'src/scss/style.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: 'src/scss/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+	grunt.registerTask('build', ['sass']);
+}

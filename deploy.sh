95dbcb1df0a413883fd66e19a7369eed419467b7
Jafnee <jafneejesmee@gmail.com>
2015-08-25 15:40:25 +0800

    Hotfix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # Set env JAFNEESITEDIR
 SRC=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-git -C $SRC pull origin master
+#git -C $SRC pull origin // Not working on server
 rsync -vd $SRC/*.html $JAFNEESITEDIR
 rsync -vd $SRC/css/*.css $JAFNEESITEDIR/css/
 rsync -vd $SRC/js/*.js $JAFNEESITEDIR/js/
